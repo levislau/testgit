0eca4e23c1dc772bf51944276d6012086c6b59ff
liushengpeng <jxlsp2007@gmail.com>
2016-10-31 15:57:43 +0800

    test 文件缓存和暂存区

--- hello.php
+++ hello.php
@@ -6,3 +6,21 @@ echo "<br/>","hello world";
 echo "hh";
 echo 1;
 echo 2;
+echo 2;
+echo 2;
+echo 2;
+echo 2;
+echo 2;
+echo 2;
+echo 2;
+echo 2;
+
+
+echo 3;
+echo 3;
+echo 3;
+echo 3;
+echo 3;
+echo 3;
+echo 3;
+echo 3;
